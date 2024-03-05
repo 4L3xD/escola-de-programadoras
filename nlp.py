7543702d59594ca01a670356e9ab93329d64c1f7
4L3xD <alessandra.olea@gmail.com>
2024-03-05 18:57:26 -0300

    add gemini and telegram integration

---
+++ nlp.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+
+# https://aistudio.google.com/app/apikey
+import env
+
+def ia(user_msg):
+    genai.configure(api_key=env.GOOGLE_API_KEY)
+
+    for m in genai.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(m.name)
+
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(user_msg)
+    msg = response.text
+
+    # Speech Analysis
+    #print(response.prompt_feedback)
+    return msg
