59c92a34a79208ecbae2c3e0c1504d091c7603c1
4L3xD <alessandra.olea@gmail.com>
2024-03-16 01:28:40 -0300

    add bot docs

---
+++ bot/README.md
@@ -0,0 +1,13 @@
+# Dependências
+```
+pip install requests &&
+pip install -q -U google-generativeai &&
+python3 -m pip install --upgrade Pillow &&
+pip install telepot
+```
+
+# Docs
+- https://telepot.readthedocs.io/en/latest/
+- https://ai.google.dev/tutorials/python_quickstart
+- https://aistudio.google.com/app/apikey
+- https://pillow.readthedocs.io/en/stable/installation.html
--- bot/geminiAI.py
+++ bot/geminiAI.py
@@ -1,10 +1,6 @@
-# https://ai.google.dev/tutorials/python_quickstart
-# curl -o image.jpeg https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQ_Kevbk21QBRy-PgB4kQpS79brbmmEG7m3VOTShAn4PecDU5H5UxrJxE3Dw1JiaG17V88QIol19-3TM2wCHw
-
 import google.generativeai as genai
 from PIL import Image
 from downloadImage import ImageProcessing
-# https://aistudio.google.com/app/apikey
 from env import GOOGLE_API_KEY
 
 class Gemini():
